af1fa1c9297a8e0c7d9152e3bfd13a8285e5a29d
Panu Oksiala <panu@oksiala.fi>
2018-12-17 23:23:18 +0200

    Implement array zip.

---
+++ test/zip.test.js
@@ -0,0 +1,82 @@
+const zip = require('./../utils').zip
+
+describe('zip', () => {
+  describe('should work with valid input', () => {
+
+    test('empty arrays',() => {
+      expect(zip([], [])).toEqual([])
+    })
+
+    test('with arrays of same size', () => {
+      const a = [1, 2]
+      const b = ['a', 'b']
+      const c = zip(a, b)
+
+      expect(c[0][0]).toBe(1)
+      expect(c[0][1]).toBe('a')
+      expect(c[1][0]).toBe(2)
+      expect(c[1][1]).toBe('b')
+    })
+  })
+
+  describe('should throw with invalid input', () => {
+    
+    test('arrays of different size', () => {
+      expect(() => 
+        zip([1, 2], [1, 2, 3])  
+      ).toThrow()
+    })
+
+    test('strings', () => {
+      expect(() => {
+        zip('asd', 'dsa')
+      }).toThrow()
+    })
+
+    test('null', () => {
+      expect(() => {
+        zip(null, null)
+      }).toThrow()
+    })
+
+    test('number', () => {
+      expect(() => {
+        zip(1, 2)
+      }).toThrow()
+    })
+
+    test('no parameters', () => {
+      expect(() => {
+        zip()
+      }).toThrow()
+    })
+
+    test('one parameter', () => {
+      expect(() => {
+        zip([])
+      }).toThrow()
+    })
+    
+    test('objects', () => {
+      expect(() => {
+        zip({}, {})
+      }).toThrow()
+    })
+
+    test('Maps', () => {
+      const a = new Map()
+      const b = new Map()
+      expect(() => {
+        zip(a, b)
+      }).toThrow()
+    })
+
+    test('Sets', () => {
+      const a = new Set()
+      const b = new Set()
+      expect(() => {
+        zip(a, b)
+      }).toThrow()
+    })
+  })
+})
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const assert = require('assert')
+
 /**
  * Convert `HH:MM` or `HH:MM AM/PM` to minutes from midnight.
  * Return Number on success or null if not recognized.
@@ -23,6 +25,20 @@ const tsToMinutesFromMidnight = (ts) => {
   }
 }
 
+const zip = (arr1, arr2) =>{
+  assert(
+    typeof(arr1) === 'object' && typeof(arr1.length) === 'number',
+    'arr1 is not a valid array'  
+  )
+  assert(
+    typeof(arr2) === 'object' && typeof(arr2.length) === 'number',
+    'arr2 is not a valid array'  
+  )
+  assert(arr1.length === arr2.length, 'Arrays must be of same size')
+  return arr1.map((e, i) => [e, arr2[i]])
+}
+
 module.exports = {
-  tsToMinutesFromMidnight
+  tsToMinutesFromMidnight,
+  zip
 }
