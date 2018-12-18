3593bc4128f7c79fb5726fbf96713f7d4dcf64c4
Panu Oksiala <panu@oksiala.fi>
2018-12-19 00:39:16 +0200

    Better integration test.

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,17 +1,23 @@
 const index = require('./../index')
 const MongoMemoryServer = require('mongodb-memory-server').default
+const shopController = require('./../controllers/shop')
+const mongoose = require('mongoose')
+const Shop = require('./../models/shop')
+
 require('dotenv').config()
 const mongoServer = new MongoMemoryServer()
+
 
 // TODO: Better integration tests
 describe('Integration test', async () => {
 
-  const context = {}
+  const context = { secrets: {} }
+  let dbUri
 
   beforeAll( async () => {
-    const dbUri = await mongoServer.getConnectionString()
-    context.DB_URL = dbUri
-    context.DATA_URL = process.env.DATA_URL
+    dbUri = await mongoServer.getConnectionString()
+    context.secrets.DB_URL = dbUri
+    context.secrets.DATA_URL = process.env.DATA_URL
   })
 
   afterAll( async () => {
@@ -19,13 +25,26 @@ describe('Integration test', async () => {
     console.log('disconnected database')
   })
 
+  beforeEach( async () => {
+    await mongoose.connect(dbUri)
+    await Shop.remove({})
+    await mongoose.disconnect()
+  })
+
   test('Should work', async () => {
-    index(context, (err, res) => {
+    await mongoose.connect(dbUri)
+    const before = await shopController.count()
+    await mongoose.disconnect()
+    await index(context, (err, res) => {
       expect(err).toBe(null)
       expect(res.jsonLength).toBeGreaterThan(0)
       expect(res.filteredLength).toBeGreaterThan(0)
       expect(res.dbCountBefore).toBe(0)
-      expect(res.dbCountBefore).toBe(res.filteredLength)
+      expect(res.dbCountAfter).toBe(res.filteredLength)
     })
-  })
+    await mongoose.connect(dbUri)
+    const after = await shopController.count()
+    await mongoose.disconnect()
+    expect(after).toBeGreaterThan(before)
+  }, 10000)
 })
